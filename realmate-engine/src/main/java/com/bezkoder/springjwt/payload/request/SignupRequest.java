631f395bec3e98cbc4b8405a13889d35de18d8ac
Valerio Montieri <v.montieri@reply.it>
2020-12-02 15:32:29 +0100

    First commit

---
+++ realmate-engine/src/main/java/com/bezkoder/springjwt/payload/request/SignupRequest.java
@@ -0,0 +1,91 @@
+package com.bezkoder.springjwt.payload.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.sql.Date;
+import java.util.Set;
+
+import javax.validation.constraints.*;
+ 
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String name;
+
+    @Size(min = 3, max = 20)
+    private String surname;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getPropic() {
+        return propic;
+    }
+
+    public void setPropic(String propic) {
+        this.propic = propic;
+    }
+
+    @NotNull
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private Date birthdate;
+
+    private String propic;
+
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+    
+    private Set<String> role;
+    
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+  
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String username) {
+        this.name = username;
+    }
+ 
+    public String getEmail() {
+        return email;
+    }
+ 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public Set<String> getRole() {
+      return this.role;
+    }
+    
+    public void setRole(Set<String> role) {
+      this.role = role;
+    }
+}
